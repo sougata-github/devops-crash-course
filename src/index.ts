d05fc4142da81b4c24a14844411d9783c32eccf2
Sougata <sougatadas9874@gmail.com>
2025-09-30 20:56:52 +0530

    move into basics.

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-console.log("Hello DevOps");
-console.log("I'm learning CI/CD using Github Actions");
--- tests/test.ts
+++
@@ -1,3 +0,0 @@
-console.log("Starting tests...");
-setTimeout(() => console.log("Waiting for 2 seconds..."), 2000);
-console.log("Starting tests...");
